14da84f6b78d14adfef275aac310e57248643a28
zhang <zhangwh754@163.com>
2024-08-17 23:11:50 +0800

    feat: result and error handle
    - return json like {"code": 200,"msg": "success","data": {}}

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import Koa from 'koa'
 import bodyParser from 'koa-bodyparser'
 import KoaRouter from 'koa-router'
-import routes from './mock/index.js'
+import routes from './app/mock/index.js'
+import * as middlewares from "./app/middlewares/index.js";
 
 const app = new Koa()
 const router = new KoaRouter()
 
 app.use(bodyParser())
+app.use(middlewares.responseHandle())
+app.use(middlewares.errorHandle())
 
 const getResponse = async (callback, ctx) => {
   return new Promise(async resolve => {
