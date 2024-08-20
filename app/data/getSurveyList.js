3da3f392b3202b113b36305133e0e3750743d84d
zhang <zhangwh754@163.com>
2024-08-20 20:04:05 +0800

    feat: 修改getSurveyList函数

--- app/data/getSurveyList.js
+++ app/data/getSurveyList.js
@@ -8,25 +8,31 @@ export default function getQuestionList(ctx) {
   const result = []
 
   if (keyword && keyword.length >= 4) {
+    const _isPublish = isPublish == 'true' ? true : Random.boolean()
+
     return [
       {
         id: Random.id(),
         title: keyword,
-        isPublish: isPublish == 'true' ? true : Random.boolean(),
+        isPublish: _isPublish,
         isStar: isStar == 'true' ? true : Random.boolean(),
         isDelete: isDelete == 'true' ? true : false,
+        answerCount: _isPublish ? Random.integer(0, 200) : 0,
         createTime: Random.datetime('yyyy-MM-dd'),
       },
     ]
   }
 
   for (let i = 0; i < pageSize; i++) {
+    const _isPublish = isPublish == 'true' ? true : Random.boolean()
+
     result.push({
       id: Random.id(),
       title: Random.ctitle(),
-      isPublish: isPublish == 'true' ? true : Random.boolean(),
+      isPublish: _isPublish,
       isStar: isStar == 'true' ? true : Random.boolean(),
       isDelete: isDelete == 'true' ? true : false,
+      answerCount: _isPublish ? Random.integer(0, 200) : 0,
       createTime: Random.datetime('yyyy-MM-dd'),
     })
   }
