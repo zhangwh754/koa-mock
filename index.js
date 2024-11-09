0fa0373a3e3f18bbefba0b90749cefc7ad9082d2
张文皓 <zhangwh754@163.com>
2024-11-09 14:03:28 +0800

    feat: 跨域调整

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import Koa from 'koa'
+import cors from '@koa/cors'
 import bodyParser from 'koa-bodyparser'
 import * as middlewares from './app/middlewares/index.js'
 import { useRoutes } from './app/router/index.js'
 
 const app = new Koa()
 
+app.use(cors())
 app.use(bodyParser())
 app.use(middlewares.responseHandle())
 app.use(middlewares.errorHandle())
